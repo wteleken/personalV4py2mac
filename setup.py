0d497f3ac339c127691655aafd547b3259cd8cfe
wteleken <willianteleken@gmail.com>
2024-07-02 15:50:19 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
 from setuptools import setup
 
-# Para o script gerar_arquivo_novo_treino.py
 setup(
-    app=['gerar_arquivo_novo_treino.py'],
-    options={'py2app': {'packages': ['openpyxl']}},
-    setup_requires=['py2app'],
-)
-
-# Para o script criar_treino.py
-setup(
-    app=['criar_treino.py'],
-    options={'py2app': {'packages': ['pandas', 'openpyxl', 'numpy', 'random']}},
-    setup_requires=['py2app'],
-)
-
-# Para o script unir_planilhas.py
-setup(
-    app=['unir_planilhas.py'],
-    options={'py2app': {'packages': ['openpyxl', 'pandas']}},
+    app=['gerar_arquivo_novo_treino.py', 'criar_treino.py', 'unir_planilhas.py'],
+    options={'py2app': {'packages': ['openpyxl', 'pandas', 'numpy', 'random']}},
     setup_requires=['py2app'],
 )
